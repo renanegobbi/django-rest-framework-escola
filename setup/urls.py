7a9fac606e25108cd82390ad32c6b01a27d1a7be
Renan Gobbi <renaneg2019@hotmail.com>
2023-02-21 14:22:13 -0300

    Adiciona documentacao da api pelo swagger e pelo redoc

--- setup/urls.py
+++ setup/urls.py
@@ -2,6 +2,22 @@ from django.contrib import admin
 from django.urls import path,include
 from escola.views import AlunosViewSet, CursosViewSet, MatriculaViewSet, ListaMatriculasAluno, ListaAlunosMatriculados
 from rest_framework import routers
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Escola",
+      default_version='v1',
+      description="Gerencia alunos e cursos da escola.",
+      terms_of_service="#",
+      contact=openapi.Contact(email="user@hotmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 router = routers.DefaultRouter()
 router.register('alunos', AlunosViewSet, basename='Alunos')
@@ -12,5 +28,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls) ),
     path('aluno/<int:pk>/matriculas/', ListaMatriculasAluno.as_view()),
-    path('curso/<int:pk>/matriculas/', ListaAlunosMatriculados.as_view())
+    path('curso/<int:pk>/matriculas/', ListaAlunosMatriculados.as_view()),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
